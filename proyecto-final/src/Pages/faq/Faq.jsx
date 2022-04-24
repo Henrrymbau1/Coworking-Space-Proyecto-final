54312c6aea6dd3b030cb89b254906da746ace98c
javier castillo <javier73castillo@hotmail.com>
2022-04-24 19:57:39 +0200

    preguntas frecuentes

--- proyecto-final/src/Pages/faq/Faq.jsx
+++ proyecto-final/src/Pages/faq/Faq.jsx
@@ -2,30 +2,32 @@ import React from "react";
 import "./faq.scss";
 
 export default function Faq() {
+
+
   return (
     <div class="container">
       <div className="container-faq">
-          <div className="faq">
-            <h1>
-              Potenciamos el trabajo colaborativo y generamos ecosistemas
-              profesionales.
-            </h1>
-          </div>
-          <div class="form-faq">
-            <form action="">
-              <h2>Solicita información sobre nuestros servicios.</h2>
-              <div class="contact">
-                <input type="text" placeholder="Tu nombre" />
-                <input type="text" placeholder="E-mail" />
-                <input type="text" placeholder="Teléfono" />
-              </div>
-              <p>
-                SPOT AT WORK tratará tus datos únicamente para tramitar tu
-                solicitud. Puedes conocer cómo ejercer tus derechos de acceso,
-                rectificación y supresión en nuestra Política de Privacidad
-              </p>
-            </form>
-          </div>
+        <div className="faq">
+          <h1>
+            Potenciamos el trabajo colaborativo y generamos ecosistemas
+            profesionales.
+          </h1>
+        </div>
+        <div class="form-faq">
+          <form action="">
+            <h2>Solicita información sobre nuestros servicios.</h2>
+            <div class="contact">
+              <input type="text" placeholder="Tu nombre" />
+              <input type="text" placeholder="E-mail" />
+              <input type="text" placeholder="Teléfono" />
+            </div>
+            <p>
+              SPOT AT WORK tratará tus datos únicamente para tramitar tu
+              solicitud. Puedes conocer cómo ejercer tus derechos de acceso,
+              rectificación y supresión en nuestra Política de Privacidad
+            </p>
+          </form>
+        </div>
       </div>
 
       <div class="container-main">
@@ -35,42 +37,96 @@ export default function Faq() {
           Los retos urgentes sociales y medioambientales a los que nos
           enfrentamos exigen que las empresas y organizaciones adopten un
           compromiso de actuación para impulsar una reactivación sostenible.</p>
-       <p> 
+        <p>
           Para poner en marcha estos cambios necesarios necesitamos nuevas ideas
           de negocio, transformar el tejido empresarial y cambiar nuestros
           hábitos y comportamientos. </p>
 
-       <p>Todo ello desde ecosistemas vivos donde la
+        <p>Todo ello desde ecosistemas vivos donde la
           colaboración sea la base. Desde nuestra experiencia a nivel global en
           los últimos 15 años, Impact Hub ofrece diferentes servicios para
           acompañar a empresas, organizaciones y administraciones en su camino
           para alcanzar los objetivos de la Agenda 2030. </p>
-          
+
       </div>
 
       {/* box para servicios */}
 
       <div>
         <div class="box-servicios">
-            <h1>Servicios Coworking </h1>
-            <h3>Buscando nuevas ideas</h3>
+          <h1>Servicios Coworking </h1>
+          <h3>Buscando nuevas ideas</h3>
         </div>
-            <div class="box-servicios-img">
-                <div class="box">
-                <img src="./assets/services1.jpg" alt="coworking"/>
-                  <p>Todo ello desde ecosistemas vivos donde la colaboración sea la base. Desde nuestra experiencia a nivel global en los últimos 15 años, Impact Hub ofrece diferentes servicios para acompañar a empresas.</p>
-                </div>
-                <div class="box">
-                <img src="./assets/services2.jpg" alt="coworking"/>
-                <p>Todo ello desde ecosistemas vivos donde la colaboración sea la base. Desde nuestra experiencia a nivel global en los últimos 15 años, Impact Hub ofrece diferentes servicios para acompañar a empresas.</p>
-                </div>
-                <div class="box">
-                <img src="./assets/services3.jpg" alt="coworking"/>
-                <p>Todo ello desde ecosistemas vivos donde la colaboración sea la base. Desde nuestra experiencia a nivel global en los últimos 15 años, Impact Hub ofrece diferentes servicios para acompañar a empresas.</p>
-                </div>
-            </div>
+        <div class="box-servicios-img">
+          <div class="box">
+            <img src="./assets/services1.jpg" alt="coworking" />
+            <p>Todo ello desde ecosistemas vivos donde la colaboración sea la base. Desde nuestra experiencia a nivel global en los últimos 15 años, Impact Hub ofrece diferentes servicios para acompañar a empresas.</p>
+          </div>
+          <div class="box">
+            <img src="./assets/services2.jpg" alt="coworking" />
+            <p>Todo ello desde ecosistemas vivos donde la colaboración sea la base. Desde nuestra experiencia a nivel global en los últimos 15 años, Impact Hub ofrece diferentes servicios para acompañar a empresas.</p>
+          </div>
+          <div class="box">
+            <img src="./assets/services3.jpg" alt="coworking" />
+            <p>Todo ello desde ecosistemas vivos donde la colaboración sea la base. Desde nuestra experiencia a nivel global en los últimos 15 años, Impact Hub ofrece diferentes servicios para acompañar a empresas.</p>
+          </div>
+        </div>
       </div>
+      {/* box para servicios */}
+
+      {/* preguntas frecuentes */}
+      <div class="box-servicios">
+          <h1>Preguntas frecuentes</h1>
+          <h3>Respondemos todas sus consultas</h3>
+        </div>
+      <div class="container-main-query">
+        <div class="box-query">
+          <p>¿Qué tan flexible son los espacios que ofrecen en sus despachos? </p>
+        </div>
+        <div class="query1" id="show">A veces no es fácil saber cuánto espacio necesitas y durante cuánto tiempo. Una alternativa podría ser contratar con un operador de espacios flexibles por una cuota mensual, en lugar de un contrato de alquiler tradicional.
+          Los espacios de oficinas flexibles llevan tiempo siendo una solución viable para autónomos, startups y trabajadores en remoto. Ahora también están ganando adeptos entre las empresas más grandes por su flexibilidad, agilidad y estructura de precios.
+        </div>
+
+        <div class="box-query">
+          <p>¿Qué tan flexible son los espacios que ofrecen en sus despachos? </p>
+          
+        </div>
+        <div class="query1" id="show">A veces no es fácil saber cuánto espacio necesitas y durante cuánto tiempo. Una alternativa podría ser contratar con un operador de espacios flexibles por una cuota mensual, en lugar de un contrato de alquiler tradicional.
+          Los espacios de oficinas flexibles llevan tiempo siendo una solución viable para autónomos, startups y trabajadores en remoto. Ahora también están ganando adeptos entre las empresas más grandes por su flexibilidad, agilidad y estructura de precios.
+        </div>
+
+        <div class="box-query">
+          <p>¿Qué tan flexible son los espacios que ofrecen en sus despachos? </p>
+          
+        </div>
+        <div class="query1" id="show">A veces no es fácil saber cuánto espacio necesitas y durante cuánto tiempo. Una alternativa podría ser contratar con un operador de espacios flexibles por una cuota mensual, en lugar de un contrato de alquiler tradicional.
+          Los espacios de oficinas flexibles llevan tiempo siendo una solución viable para autónomos, startups y trabajadores en remoto. Ahora también están ganando adeptos entre las empresas más grandes por su flexibilidad, agilidad y estructura de precios.
+        </div>
+        <div class="box-query">
+          <p>¿Qué tan flexible son los espacios que ofrecen en sus despachos? </p>
+         
+        </div>
+        <div class="query1" id="show">A veces no es fácil saber cuánto espacio necesitas y durante cuánto tiempo. Una alternativa podría ser contratar con un operador de espacios flexibles por una cuota mensual, en lugar de un contrato de alquiler tradicional.
+          Los espacios de oficinas flexibles llevan tiempo siendo una solución viable para autónomos, startups y trabajadores en remoto. Ahora también están ganando adeptos entre las empresas más grandes por su flexibilidad, agilidad y estructura de precios.
+        </div>
+        <div class="box-query">
+          <p>¿Qué tan flexible son los espacios que ofrecen en sus despachos? </p>
+         
+        </div>
+        <div class="query1" id="show">A veces no es fácil saber cuánto espacio necesitas y durante cuánto tiempo. Una alternativa podría ser contratar con un operador de espacios flexibles por una cuota mensual, en lugar de un contrato de alquiler tradicional.
+          Los espacios de oficinas flexibles llevan tiempo siendo una solución viable para autónomos, startups y trabajadores en remoto. Ahora también están ganando adeptos entre las empresas más grandes por su flexibilidad, agilidad y estructura de precios.
+        </div>
+        <div class="box-query">
+          <p>¿Qué tan flexible son los espacios que ofrecen en sus despachos? </p>
+          
+        </div>
+        <div class="query1" id="show">A veces no es fácil saber cuánto espacio necesitas y durante cuánto tiempo. Una alternativa podría ser contratar con un operador de espacios flexibles por una cuota mensual, en lugar de un contrato de alquiler tradicional.
+          Los espacios de oficinas flexibles llevan tiempo siendo una solución viable para autónomos, startups y trabajadores en remoto. Ahora también están ganando adeptos entre las empresas más grandes por su flexibilidad, agilidad y estructura de precios.
+        </div>
+      </div>
+
+
     </div>
-    
+
   );
 }
